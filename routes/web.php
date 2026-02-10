2124704e687394eec8d7780b40e457b3b390dcc6
Cristian Cogollo <ghernandez339@unab.edu.co>
2026-02-10 14:32:12 -0500

    readme

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/mi-ruta-app', function () {
+    return "HOLA ESTA ES MI RUTA EN LARAVEL";
+});
