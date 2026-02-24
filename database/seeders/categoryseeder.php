a6918c9a595f75fefcb47ba82c39ae590360c273
Laboratorio de Telecomunicaciones <cisco@academico.unab.ad>
2026-02-24 14:58:18 -0500

    creacion de seeders

---
+++ database/seeders/categoryseeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class categoryseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tecnologia = new category();
+        $tecnologia->name = 'Tecnología';
+        $tecnologia->description = "todo lo relacionado";
+        $tecnologia->save();
+    }
+}
